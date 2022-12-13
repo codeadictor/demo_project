7741e882a0a3c973ca57e6f8aab3c1bb46faf142
Usman_Ali_Ansari <usmantoyou@gmail.com>
2022-12-13 11:06:27 +0530

    after fill array of 20000 we sorted the array

---
+++ sorted_array.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// random number function 
+
+int randomNumber(){
+
+        int random_number = 1+rand()%10000;
+         return random_number;
+
+}
+
+////////////***First Approach***////////////
+ void solve(){
+
+    
+   
+    int arr[20000+1];
+    srand(time(0));
+    for(int i = 0;i<20000;i++){
+        arr[i] = randomNumber();
+    
+    }
+    
+    sort(arr,arr+20000);
+
+    for(int i = 0;i<20000;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
+
+//////////////***Main Function***//////////////
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
