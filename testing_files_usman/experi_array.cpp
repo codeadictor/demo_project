da2b82b2fecdef74e2a05f2802c3b1813656c4b6
Usman_Ali_Ansari <usmantoyou@gmail.com>
2022-12-13 22:37:07 +0530

    file move to thier respective folder

---
+++ testing_files_usman/experi_array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i = 0;i<n;i++)
+		cin>>arr[i];
+
+	// print the array that value have product of 2;
+
+
+	
+	for(int i = 0;i<n;i++)
+		cout<<arr[i]*2<<" ";
+
+
+	cout<<endl;
+
+
+	// after done in feature we want to experiment some more feature of arra
+	// but the question is what is it
+	return 0;
+}
